d64ff3ea50ee36677807c97b42a099cbc025ee60
Vineet Jain <jain.vinee@gmail.com>
2016-01-03 23:49:54 -0800

    initial runner.py

---
+++ runner.py
@@ -0,0 +1,13 @@
+"""
+Runner script that contains the main method. It is from this method where
+everything begins.
+"""
+__author__ = "Vineet Jain"
+
+
+def main():
+    print 'Hello World'
+
+
+if __name__ == "__main__":
+    main()
