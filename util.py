41457f2c48530e1c61a628cbc8de9ccaaf9e82d9
Vineet Jain <jain.vinee@gmail.com>
2016-01-06 23:19:18 -0800

    file_exists method in util.py

--- util.py
+++ util.py
@@ -3,8 +3,22 @@ Utility functions
 """
 __author__ = "Vineet Jain"
 
+import os
+
 
 def read_file(filename):
     with open(filename, "rb") as f:
         contents = f.readlines()
     return [line.rstrip() for line in contents]
+
+
+def file_exists(path):
+    """
+    Checks if the specified path actually exists as a file or not
+    :param path: the path in question
+    :return: True if the path is a file and it exists, False otherwise
+    """
+    if os.path.exists(path):
+        return os.path.isfile(path)
+    else:
+        return False
