571de54e662eadb9f419e7b6757d97680084fc9d
Vineet Jain <jain.vinee@gmail.com>
2016-01-11 23:56:34 -0800

    comments for read_file, added another utility method for reading file into one string

--- util.py
+++ util.py
@@ -7,10 +7,25 @@ import os
 
 
 def read_file(filename):
+    """
+    Reads a specified file into a list of lines
+    :param filename: the path to the file we want to read
+    :return: a list of lines where the ith element in the list is the ith
+    line in the file
+    """
     with open(filename, "rb") as f:
         contents = f.readlines()
     return [line.rstrip() for line in contents]
 
+
+def read_file_str(filename):
+    """
+    Reads a specified file into one large string
+    :param filename: the path to the file we want to read
+    :return: one string containing all the content of the file where each
+    line is separated by a newline character
+    """
+    return "\n".join(read_file(filename))
 
 def file_exists(path):
     """
