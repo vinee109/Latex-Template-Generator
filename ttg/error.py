3c62925ca3f0bdb2204403ba6bffb5e7829b0999
Vineet Jain <jain.vinee@gmail.com>
2016-01-11 23:57:20 -0800

    MissingQuestionLayout exception

--- ttg/error.py
+++ ttg/error.py
@@ -3,7 +3,7 @@ error.py
 """
 __author__ = "Vineet Jain"
 
-class ConfigFileDoesNotExistException(Exception):
+class FileDoesNotExistException(Exception):
     def __init__(self, filename):
         self.message = "{} could not be found.".format(filename)
 
@@ -13,3 +13,9 @@ class MalformedConfigFileException(Exception):
     def __init__(self, filename):
         self.message = "{} contains malformed syntax as a configuration " \
                   "file".format(filename)
+
+
+class MissingQuestionLayoutFile(Exception):
+
+    def __init__(self, filename):
+        self.message = "{} does not exist as a question layout file".format(filename)
